7c49271f3e8f9d8a71c0eb0cd857ae5a93ee62d3
darkmister <damian132255@gmail.com>
2026-05-29 16:53:57 +0200

    Step 5 (GREEN): Add validation and throw exception for negative numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
@@ -9,16 +11,26 @@ public class StringCalculator {
         String delimiter = ",|\n";
         if (numbers.startsWith("//")) {
             int delimiterIndex = numbers.indexOf("\n");
-            // Wyciągamy separator i cytujemy go na wypadek znaków specjalnych (np. typu *)
             delimiter = Pattern.quote(numbers.substring(2, delimiterIndex));
             numbers = numbers.substring(delimiterIndex + 1);
         }
 
         String[] tokens = numbers.split(delimiter);
         int sum = 0;
+        List<Integer> negatives = new ArrayList<>();
+
         for (String token : tokens) {
-            sum += Integer.parseInt(token);
+            int num = Integer.parseInt(token);
+            if (num < 0) {
+                negatives.add(num);
+            }
+            sum += num;
         }
+
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("negatives not allowed: " + negatives);
+        }
+
         return sum;
     }
 }
