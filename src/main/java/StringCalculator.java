86fe34e2f58efd588e2c1bcc38b87be57fce2e28
darkmister <damian132255@gmail.com>
2026-05-29 16:53:21 +0200

    Step 4 (GREEN): Parse custom delimiter configuration from string header

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,11 +1,20 @@
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
 
-        // Rozdzielamy po przecinku LUB nowej linii
-        String[] tokens = numbers.split(",|\n");
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            // Wyciągamy separator i cytujemy go na wypadek znaków specjalnych (np. typu *)
+            delimiter = Pattern.quote(numbers.substring(2, delimiterIndex));
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
+
+        String[] tokens = numbers.split(delimiter);
         int sum = 0;
         for (String token : tokens) {
             sum += Integer.parseInt(token);
